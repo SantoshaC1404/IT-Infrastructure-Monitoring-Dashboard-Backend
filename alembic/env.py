df428d2cf9e3777385ceeef3bd7c0552edf6941a
SantoshaC1404 <santosha1404@gmail.com>
2026-07-05 21:44:12 +0530

    alembic configuration

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.core.config import settings
+from app.db.base import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +21,16 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+
+# Set the database URL from your application settings
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL,
+)
+
+# target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +76,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- app/db/base.py
+++ app/db/base.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import DeclarativeBase
 
+# Import all models
+import app.models
+
 
 class Base(DeclarativeBase):
     pass

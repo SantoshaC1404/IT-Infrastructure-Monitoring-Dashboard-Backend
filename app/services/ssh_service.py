056425cc8db5954ab0a007d9cf2d7f11c87213d2
SantoshaC1404 <santosha1404@gmail.com>
2026-07-12 15:17:30 +0530

    updated ssh-service and created linux agent

--- app/services/ssh_service.py
+++ app/services/ssh_service.py
@@ -6,6 +6,40 @@ from app.models import Server
 
 class SSHService:
 
+    def __init__(
+        self,
+        hostname: str,
+        username: str,
+        password: str,
+        port: int = 22,
+    ):
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.port = port
+        self.client = None
+
+    def connect(self):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        self.client.connect(
+            hostname=self.hostname,
+            username=self.username,
+            password=self.password,
+            port=self.port,
+            timeout=5,
+        )
+
+    def execute(self, command: str) -> str:
+        stdin, stdout, stderr = self.client.exec_command(command)
+
+        return stdout.read().decode().strip()
+
+    def close(self):
+        if self.client:
+            self.client.close()
+
     @staticmethod
     def test_connection(
         hostname: str,

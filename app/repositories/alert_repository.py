a4960c680cb53593774d0122c995714634644c5b
SantoshaC1404 <santosha1404@gmail.com>
2026-07-13 06:05:34 +0530

    add alert repo and constants

---
+++ app/repositories/alert_repository.py
@@ -0,0 +1,18 @@
+class AlertRepository:
+
+    def create(self, alert): ...
+
+    def get_open_alert(
+        self,
+        server_id,
+        metric,
+    ): ...
+
+    def resolve_alert(
+        self,
+        alert,
+    ): ...
+
+    def latest(self): ...
+
+    def unresolved(self): ...

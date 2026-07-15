2c3df148b0726491dfa814e9bd7655df6a48da9a
SantoshaC1404 <santosha1404@gmail.com>
2026-07-15 21:04:56 +0530

    "models updated"

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,26 @@
-from app.models.user import User
-from app.models.server import Server
-from app.models.monitoring_snapshot import MonitoringSnapshot
-from app.models.alert import Alert
+# from app.models.user import User
+# from app.models.server import Server
+# from app.models.monitoring_snapshot import MonitoringSnapshot
+# from app.models.alert import Alert
+
+from .user import User
+from .server import Server
+from .server_inventory import ServerInventory
+from .monitoring_snapshot import MonitoringSnapshot
+from .alert import Alert
+from .server_health import ServerHealth
+
+# from .monitored_service import MonitoredService
+from .network_interface import NetworkInterface
+from .disk import Disk
+
+__all__ = [
+    "User",
+    "Server",
+    "ServerInventory",
+    "MonitoringSnapshot",
+    "Alert",
+    "NetworkInterface",
+    "Disk",
+    "ServerHealth",
+]

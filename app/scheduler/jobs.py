d97c3715caf6a5425a4c8a65aae7312441b872d4
SantoshaC1404 <santosha1404@gmail.com>
2026-07-16 05:44:47 +0530

    "schedular updated"

--- app/scheduler/jobs.py
+++ app/scheduler/jobs.py
@@ -1,7 +1,6 @@
 import logging
 
 from app.db.session import SessionLocal
-from app.repositories.server_repository import ServerRepository
 from app.services.monitoring_service import MonitoringService
 
 logger = logging.getLogger(__name__)
@@ -13,30 +12,11 @@ def collect_metrics_job():
 
     try:
 
-        server_repo = ServerRepository(db)
+        logger.info("Starting scheduled metrics collection")
 
         monitoring = MonitoringService(db)
 
-        servers = server_repo.get_monitoring_enabled()
-
-        logger.info(
-            "Collecting metrics for %s servers",
-            len(servers),
-        )
-
-        for server in servers:
-
-            try:
-
-                monitoring.collect(server)
-
-            except Exception as ex:
-
-                logger.exception(
-                    "Failed collecting metrics for %s : %s",
-                    server.hostname,
-                    ex,
-                )
+        monitoring.monitor_all_servers()
 
     finally:
 

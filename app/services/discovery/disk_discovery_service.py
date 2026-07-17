ba2490af2cb64dc75ffc377a841c81ff98b3d19d
SantoshaC1404 <santosha1404@gmail.com>
2026-07-17 06:10:26 +0530

    "updated discovery services"

---
+++ app/services/discovery/disk_discovery_service.py
@@ -0,0 +1,56 @@
+from app.commands.discovery.factory import DiscoveryCommandFactory
+from app.schemas.disk import DiskBase
+from app.services.ssh_service import SSHService
+from app.utils.enums import ServerType
+
+
+class DiskDiscoveryService:
+
+    def __init__(
+        self,
+        ssh: SSHService,
+        server_type: ServerType,
+    ):
+        self.ssh = ssh
+        self.commands = DiscoveryCommandFactory.get(server_type)
+        self.server_type = server_type
+
+    def discover(self) -> list[DiskBase]:
+
+        if self.server_type == ServerType.LINUX:
+            return self._discover_linux()
+
+        return self._discover_windows()
+
+    def _discover_linux(self):
+
+        output = self.ssh.execute(self.commands.disk_inventory())
+
+        disks = []
+
+        for line in output.splitlines():
+
+            values = {}
+
+            for item in line.split():
+
+                key, value = item.split("=")
+
+                values[key] = value.replace('"', "")
+
+            disks.append(
+                DiskBase(
+                    device_name=values.get("NAME"),
+                    filesystem=values.get("FSTYPE"),
+                    mount_point=values.get("MOUNTPOINT"),
+                    total_bytes=int(values.get("SIZE", 0)),
+                    used_bytes=0,
+                    free_bytes=0,
+                )
+            )
+
+        return disks
+
+    def _discover_windows(self):
+        # Implement parsing of WMIC output
+        return []
---
+++ app/services/discovery/network_interface_discovery_service.py
@@ -0,0 +1,53 @@
+from app.commands.discovery.factory import DiscoveryCommandFactory
+from app.schemas.network_interface import NetworkInterfaceBase
+from app.services.ssh_service import SSHService
+from app.utils.enums import ServerType
+
+
+class NetworkInterfaceDiscoveryService:
+
+    def __init__(
+        self,
+        ssh: SSHService,
+        server_type: ServerType,
+    ):
+        self.ssh = ssh
+        self.commands = DiscoveryCommandFactory.get(server_type)
+        self.server_type = server_type
+
+    def discover(self) -> list[NetworkInterfaceBase]:
+
+        if self.server_type == ServerType.LINUX:
+            return self._discover_linux()
+
+        return self._discover_windows()
+
+    def _discover_linux(self):
+
+        output = self.ssh.execute(self.commands.network_interfaces())
+
+        interfaces = []
+
+        for line in output.splitlines():
+
+            values = line.split()
+
+            if len(values) < 2:
+                continue
+
+            interfaces.append(
+                NetworkInterfaceBase(
+                    interface_name=values[0],
+                    ipv4_address=values[1].split("/")[0],
+                    ipv6_address=None,
+                    mac_address=None,
+                    speed_mbps=None,
+                    is_up=True,
+                )
+            )
+
+        return interfaces
+
+    def _discover_windows(self):
+        # Implement WMIC parsing
+        return []

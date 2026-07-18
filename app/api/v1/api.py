00af6d4f56a859a0871e05040d18c18a4d5b8919
SantoshaC1404 <santosha1404@gmail.com>
2026-07-18 22:16:47 +0530

    "routes updated"

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1.routes import auth, health, servers
+from app.api.v1.routes import auth_routes, device_routes, health_routes
 
 api_router = APIRouter()
 
-api_router.include_router(health.router)
+api_router.include_router(health_routes.router)
 
-api_router.include_router(auth.router)
+api_router.include_router(auth_routes.router)
 
-api_router.include_router(servers.router)
+api_router.include_router(device_routes.router)

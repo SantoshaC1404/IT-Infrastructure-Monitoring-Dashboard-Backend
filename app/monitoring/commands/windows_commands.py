57efe603ba377d89c3835efde672d4e70e61a3a9
SantoshaC1404 <santosha1404@gmail.com>
2026-07-21 19:35:31 +0530

    "monitoring updated"

--- app/monitoring/commands/windows_commands.py
+++ app/monitoring/commands/windows_commands.py
@@ -4,38 +4,56 @@ from app.monitoring.commands.base import BaseMonitoringCommandSet
 class WindowsMonitoringCommandSet(BaseMonitoringCommandSet):
 
     def cpu_usage(self):
-        return "(Get-Counter '\\Processor(_Total)\\% Processor Time').CounterSamples.CookedValue"
+        return (
+            "powershell "
+            '"Get-CimInstance Win32_Processor | '
+            'Select-Object -ExpandProperty LoadPercentage"'
+        )
 
     def memory_usage(self):
-        return """
-        $os = Get-CimInstance Win32_OperatingSystem
-        @{
-            Total = $os.TotalVisibleMemorySize
-            Free = $os.FreePhysicalMemory
-        } | ConvertTo-Json -Compress
-        """
+        return (
+            "powershell "
+            '"Get-CimInstance Win32_OperatingSystem | '
+            'Select FreePhysicalMemory,TotalVisibleMemorySize"'
+        )
 
     def disk_usage(self):
-        return """
-        Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3" |
-        Select-Object DeviceID, Size, FreeSpace |
-        ConvertTo-Json -Compress
-        """
-
-    def network_usage(self):
-        return """
-        Get-NetAdapterStatistics |
-        Select-Object Name, ReceivedBytes, SentBytes |
-        ConvertTo-Json -Compress
-        """
+        return (
+            "powershell " "\"Get-CimInstance Win32_LogicalDisk -Filter 'DriveType=3'\""
+        )
 
     def uptime(self):
-        return """
-        ((Get-Date) - (Get-CimInstance Win32_OperatingSystem).LastBootUpTime).TotalSeconds
-        """
+        return (
+            "powershell "
+            '"(Get-Date) - '
+            '(gcim Win32_OperatingSystem).LastBootUpTime"'
+        )
+
+    def network_usage(self):
+        return (
+            "powershell "
+            '"Get-CimInstance Win32_PerfFormattedData_Tcpip_NetworkInterface | '
+            'Select-Object Name,BytesReceivedPersec,BytesSentPersec"'
+        )
 
     def load_average(self):
-        return ""
+        return (
+            "powershell "
+            '"(Get-CimInstance Win32_PerfFormattedData_PerfOS_System).ProcessorQueueLength"'
+        )
 
     def process_count(self):
-        return "(Get-Process).Count"
+        return "powershell " '"(Get-Process).Count"'
+
+    def disk_io(self):
+        return (
+            "powershell "
+            '"Get-CimInstance Win32_PerfFormattedData_PerfDisk_LogicalDisk -Filter \\"Name=\'_Total\'\\" | '
+            'Select-Object DiskReadBytesPersec,DiskWriteBytesPersec"'
+        )
+
+    def logged_in_users(self):
+        return (
+            "powershell "
+            '"(Get-CimInstance Win32_ComputerSystem).NumberOfLoggedOnUsers"'
+        )

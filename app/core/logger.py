b25fe0f153b75558ac3e3a517a848d49b17164f0
SantoshaC1404 <santosha1404@gmail.com>
2026-08-05 20:32:27 +0530

    cores updated

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,19 +1,3 @@
-"""
-import logging
-
-from app.core.config import settings
-
-
-def setup_logging():
-    logging.basicConfig(
-        level=settings.LOG_LEVEL,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-    )
-
-
-logger = logging.getLogger("it-monitoring")
-"""
-
 import logging
 
 from app.core.logging_config import LOGGING
--- app/core/security/test.py
+++
@@ -1,8 +0,0 @@
-from app.core.security.security import hash_password
-
-password = "Admin@123"
-
-print(password)
-print(len(password.encode()))
-
-print(hash_password(password))

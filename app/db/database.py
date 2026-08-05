b41c58b17088116b4c6b34603b6b6c0e498ba5ef
SantoshaC1404 <santosha1404@gmail.com>
2026-08-05 20:33:03 +0530

    db updated

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
 from app.core.config import settings
 
-# engine = create_engine(
-#     settings.DATABASE_URL.replace("%", "%%"), pool_pre_ping=True, echo=settings.DEBUG
-# )
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, echo=False)
 
 print("Database URL:", settings.DATABASE_URL)

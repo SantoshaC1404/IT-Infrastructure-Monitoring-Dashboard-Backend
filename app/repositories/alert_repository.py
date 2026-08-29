0c9901972d0ab49e9dd9d61ff42ad8db29383be8
SantoshaC1404 <santosha1404@gmail.com>
2026-08-29 06:01:27 +0530

    device alert repo updated

--- app/repositories/alert_repository.py
+++ app/repositories/alert_repository.py
@@ -1,18 +1,95 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from app.models.device_alert import Alert
+from app.utils.enums import AlertStatus
+
+
 class AlertRepository:
 
-    def create(self, alert): ...
+    def __init__(self, db: Session):
+        self.db = db
 
-    def get_open_alert(
+    # CREATE
+    def create(self, alert: Alert):
+
+        self.db.add(alert)
+        self.db.flush()
+
+        return alert
+
+    # GET OPEN ALERT FOR DEVICE + METRIC
+    def get_open_alerts(
         self,
-        device_id,
-        metric,
-    ): ...
+        device_id: int,
+        metric: str,
+    ):
+        statement = (
+            select(Alert)
+            .where(
+                Alert.device_id == device_id,
+                Alert.metric == metric,
+                Alert.status.in_(
+                    [
+                        AlertStatus.OPEN,
+                        AlertStatus.ACKNOWLEDGED,
+                    ]
+                ),
+            )
+            .order_by(Alert.created_at.desc())
+        )
 
-    def resolve_alert(
-        self,
-        alert,
-    ): ...
+        return self.db.scalars(statement).first()
 
-    def latest(self): ...
+    # LATEST ALERTS
+    def latest(self, limit: int = 20):
+        stmt = select(Alert).order_by(Alert.created_at.desc()).limit(limit)
 
-    def unresolved(self): ...
+        return list(self.db.scalars(stmt).all())
+
+    # OPEN ALERTS
+    def unresolved(self):
+
+        stmt = (
+            select(Alert)
+            .where(
+                Alert.status.in_(
+                    [
+                        AlertStatus.OPEN,
+                        AlertStatus.ACKNOWLEDGED,
+                    ]
+                )
+            )
+            .order_by(Alert.created_at.desc())
+        )
+
+        return list(self.db.scalars(statement=stmt).all())
+
+    # ACKNOWLEDGE
+    def acknowledge(self, alert: Alert):
+
+        alert.status = AlertStatus.ACKNOWLEDGED
+        alert.acknowledged_at = datetime.utcnow()
+
+        self.db.flush()
+
+        return alert
+
+    # RESOLVE
+    def resolve(self, alert: Alert):
+
+        alert.status = AlertStatus.RESOLVED
+        alert.resolved_at = datetime.utcnow()
+
+        self.db.flush()
+
+        return alert
+
+    # GET BY ID
+    def get_by_id(self, alert_id: int):
+
+        stmt = select(Alert).where(Alert.id == alert_id)
+
+        return self.db.scalars(stmt).first()
